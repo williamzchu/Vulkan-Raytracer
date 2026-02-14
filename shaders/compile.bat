5a14a4624a377e32966f964835d19043fbcf41f2
williamzchu <williamzchu@gmail.com>
2026-02-14 01:40:31 -0800

    working renderer

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,1 +1,1 @@
-C:\VulkanSDK\1.4.328.1\Bin\slangc.exe shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o slang.spv
+C:\VulkanSDK\1.4.335.0\Bin\slangc.exe shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o slang.spv
